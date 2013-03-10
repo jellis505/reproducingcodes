42425b45749f9a6a020d2c586aac04da23f650a8
Joe Ellis <joe.ellis.505@gmail.com>
2013-03-09 19:09:20 -0500

    Finished the implementation for this work

--- CODE/osr_script.m
+++ CODE/osr_script.m
@@ -4,7 +4,22 @@
 % Train the ranking function should be right here
 % This portion of the code needs to have some ground truth data labeled and
 % the relative similarities finished
-[O,S] = Create_O_and_S_Mats(category_order,used_for_training,class_labels,8);
+
+% These are the unseen classes
+
+% This is the number of iteratins we want to do
+
+
+
+accuracy 
+for iter = 1:20
+
+
+
+unseen_list = 
+unseen = [];
+
+[O,S] = Create_O_and_S_Mats2(category_order,used_for_training,class_labels,8,unseen);
 
 % Now we need to train the ranking function, but we have some values in the
 % matrices that will not correspond to the anything becuase some attributes
@@ -33,8 +48,8 @@ for l = 1:6
     
     % Now set up the cost matrices both are initialized to 0.1 in the
     % Relative Attributes paper from 2011;
-    Costs_for_O = 0.1*ones(O_length,1);
-    Costs_for_S = 0.1*ones(S_length,1);
+    Costs_for_O = .1*ones(O_length,1);
+    Costs_for_S = .1*ones(S_length,1);
     
     w = ranksvm_with_sim(osr_gist_Mat,O(1:O_length,:,l),S(1:S_length,:,l),Costs_for_O,Costs_for_S);
     weights(:,l) = w;
@@ -47,7 +62,7 @@ relative_att_predictions = osr_gist_Mat*weights;
 Train_samples = GetTrainingSample_per_category(relative_att_predictions,class_labels,used_for_training);
 
 % Calculate the means and covariances from the samples
-[means, Covariances] = meanandvar_forcat(Train_samples);
+[means, Covariances] = meanandvar_forcat(Train_samples,unseen,category_order,8);
 
 % Classify the predicted features from the system
 accuracy = BayesClass_RelAtt(relative_att_predictions,class_labels,means,Covariances,used_for_training);
